954fb72861971f90074efddbda2e2d8a8ad93d4d
Sagar <sagardhaduk1@gmail.com>
2024-07-18 20:01:20 -0500

    model evaluation added and mlflow added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from cnnClassifier.constants import *
 import os
-from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig
+from cnnClassifier.utils.common import read_yaml, create_directories, save_json
+from cnnClassifier.entity.config_entity import DataIngestionConfig, PrepareBaseModelConfig, TrainingConfig, EvaluationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -14,6 +14,8 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
     
+
+
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -27,6 +29,7 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
 
 
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
@@ -47,6 +50,8 @@ class ConfigurationManager:
 
         return prepare_base_model_config
     
+
+
     def get_training_config(self) -> TrainingConfig:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
@@ -68,3 +73,16 @@ class ConfigurationManager:
         )
 
         return training_config
+
+
+
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/sdhaduk/Kidney-Tumor-Classification.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
